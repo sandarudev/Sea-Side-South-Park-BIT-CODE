be155d14eb18ecfb8a40482056a684200fba6d48
Rashmika Helanjith <kevinrashmika@users.noreply.github.com>
2020-08-24 09:45:02 +0530

    Sign_up and js

---
+++ js/signup.js
@@ -0,0 +1,13 @@
+function validateForm() {
+  {
+    var email = document.myForm.email_tbx.value;
+    var password = document.myForm.password_tbx.value;
+    var repassword = document.myForm.re_pass_tbx.value;
+
+    //email validation
+
+    //is empty validation
+
+    //equal passwaord with re enter password
+  }
+}
